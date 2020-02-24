f02f0b00c982594572244e9055765553ab4d5723
DjCooGie <evansonchagwa01@gmail.com>
2020-02-24 17:34:52 +0300

    create index view function

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login')
+def index(request):
+   
+    return render(request,'index.html')
+
+
