c795bf0e3bb9d16e2ed5f4e7071d35c76a8702ae
DjCooGie <evansonchagwa01@gmail.com>
2020-02-24 09:00:22 +0300

    add database configs in settings

--- myhood/settings.py
+++ myhood/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'myhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'postgresql'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'moringa',
+        'PASSWORD': 'hahaa',
     }
 }
 
