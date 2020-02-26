3c232b049c82ace0bc8a345a3974dae2ab3820b7
DjCooGie <evansonchagwa01@gmail.com>
2020-02-27 02:20:35 +0300

    create BusinessForm function

--- hood/forms.py
+++ hood/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from pyuploadcare.dj.forms import ImageField
-from .models import Neighbourhood, Profile
+from .models import Neighbourhood, Profile, Business, Post
 
 
 class SignupForm(UserCreationForm):
@@ -16,3 +16,10 @@ class HoodForm(forms.ModelForm):
     class Meta:
         model = Neighbourhood
         exclude = ('admin',)
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user','neighbourhood')
+
+
