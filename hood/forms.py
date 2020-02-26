da4a7240c91b64788665902a86db9fc7731d4895
DjCooGie <evansonchagwa01@gmail.com>
2020-02-26 14:01:02 +0300

    create HoodForm function that is responsible for adding hoods

--- hood/forms.py
+++ hood/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from pyuploadcare.dj.forms import ImageField
+from .models import Neighbourhood, Profile
 
 
 class SignupForm(UserCreationForm):
@@ -9,3 +11,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
