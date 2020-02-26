49450dc6ead45299c717b87a485c25d083c05f44
DjCooGie <evansonchagwa01@gmail.com>
2020-02-26 14:05:40 +0300

    create add-hood view function

--- hood/views.py
+++ hood/views.py
@@ -3,15 +3,16 @@ from django.contrib.auth import login,authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from .forms import SignupForm
-from . import views
+from .forms import SignupForm, HoodForm
+from .models import Neighbourhood
 
 # Create your views here.
 
 @login_required(login_url='/accounts/login')
 def index(request):
-   
-    return render(request,'index.html')
+    
+
+    return render(request,'index.html',context)
 
 def signup(request):
     if request.method == 'POST':
@@ -28,5 +29,19 @@ def signup(request):
     return render(request, 'registration/registration_form.html', {'form': form})
 
 def home(request):
-    
+   
     return render(request,'home.html')
+
+def add_hood(request):
+    if request.method =="POST":
+        form  = HoodForm(request.POST,request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('index')
+    else:
+        form = HoodForm()
+    return render(request,'new-hood.html',{'form':form})
+
+
