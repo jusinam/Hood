182d123d9b1db7bf56ed1bf1fe3c87cae893471a
DjCooGie <evansonchagwa01@gmail.com>
2020-02-27 02:14:59 +0300

    create Business model class

--- hood/models.py
+++ hood/models.py
@@ -43,3 +43,21 @@ class Profile(models.Model):
         
     def __str__(self):
         return f"{self.user.username} profile"
+
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=90)
+    desciption = models.TextField(blank=True)
+    neighbourhood  = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,related_name="business")
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="owner")
+
+    def create_business(self):
+        return self.save()
+
+
+    def delete_business(self):
+        return self.delete()
+
+    def __str__(self):
+        return f"{self.name} Business"
+
