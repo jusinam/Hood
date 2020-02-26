0567732a955e20a1b10a3f84aab594aeeccd8ba7
DjCooGie <evansonchagwa01@gmail.com>
2020-02-27 02:26:20 +0300

    register business ans post models

--- hood/admin.py
+++ hood/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Neighbourhood, Profile
+from .models import Neighbourhood, Profile, Business, Post
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
